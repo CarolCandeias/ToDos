494925229bf512f8e176d8900af944f0a3c36294
Carol Candeias <carolsilva.ac565@gmail.com>
2022-06-20 22:34:12 -0300

    Adicionando projeto completo

---
+++ ToDo2.py
@@ -0,0 +1,61 @@
+print ('Pesquise a cidade desejada de acordo com os códigos abaixo: \n\n Aracaju - 79 \n Campo Grande - 67 \n Belém - 91 \n Belo Horizonte - 31 \n Boa Vista - 95 \n Brasília - 61 \n Cuiabá - 65 \n Curitiba - 41 \n Florianópolis - 48 \n Fortaleza - 85 \n Goiânia - 62 \n João Pessoa - 83 \n Macapá - 96 \n Maceió - 82 \n Manaus - 92 \n Natal - 84 \n Palmas - 63 \n Porto Alegre - 51 \n Porto Velho - 69 \n Recife - 81 \n Rio Branco - 68 \n Rio de Janeiro - 21 \n Salvador - 71 \n São Luís - 98 \n São Paulo - 11 \n Teresina - 86 \n Vitória - 27')
+
+Cidade = (input ('\nDigite a cidade desejada: '))   
+
+
+if (Cidade == '79'):
+    print ('\nVeja  os dados de Aracaju! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 664.908 habitantes  \n Tamanho da amostra sugerida - 2.393 respondentes')
+elif (Cidade == '67'):
+    print ('\nVeja  os dados de Campo Grande! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 906.098 habitantes \n Tamanho da amostra sugerida - 2.395 respondentes')
+elif (Cidade == '91'):
+    print ('\nVeja  os dados de Belém! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 1.499.641 habitantes \n Tamanho da amostra sugerida - 2.398 respondentes')
+elif (Cidade == '31'):
+    print ('\nVeja  os dados de Belo Horizonte! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 2.521.564 habitantes \n Tamanho da amostra sugerida - 2.399 respondentes')
+elif (Cidade == '95'):
+    print ('\nVeja  os dados de Boa Vista! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 419.652 habitantes \n Tamanho da amostra sugerida - 2.388 respondentes')
+elif (Cidade == '61'):
+    print ('\nVeja  os dados de Brasília! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 3.055.149 habitantes \n Tamanho da amostra sugerida - 2.400 respondentes')
+elif (Cidade == '65'):
+    print ('\nVeja  os dados de Cuiabá! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 618.124 habitantes \n Tamanho da amostra sugerida - 2.392 respondentes')
+elif (Cidade == '41'):
+    print ('\nVeja  os dados de Curitiba! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 1.948.626 habitantes \n Tamanho da amostra sugerida - 2.399 respondentes')
+elif (Cidade == '48'):
+    print ('\nVeja  os dados de Florianópolis! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 508.826 habitantes \n Tamanho da amostra sugerida - 2.390 respondentes')
+elif (Cidade == '85'):
+    print ('\nVeja  os dados de Fortaleza! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 2.686.612 habitantes \n Tamanho da amostra sugerida - 2.399 respondentes')
+elif (Cidade == '62'):
+    print ('\nVeja  os dados de Goiânia! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 1.536.097 habitantes \n Tamanho da amostra sugerida - 2.398 respondentes')
+elif (Cidade == '83'):
+    print ('\nVeja  os dados de João Pessoa! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 817.511 habitantes \n Tamanho da amostra sugerida - 2.394 respondentes')
+elif (Cidade == '96'):
+    print ('\nVeja  os dados de Macapá! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 512.902 habitantes \n Tamanho da amostra sugerida - 2.390 respondentes')
+elif (Cidade == '82'):
+    print ('\nVeja os dados de Maceió! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 1.025.360 habitantes \n Tamanho da amostra sugerida - 2.396 respondentes')
+elif (Cidade == '92'):
+    print ('\nVeja os dados de Manaus! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 2.219.580 habitantes \n Tamanho da amostra sugerida - 2.399 respondentes')
+elif (Cidade == '84'):
+    print ('\nVeja os dados de Natal! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 890.480 habitantes \n Tamanho da amostra sugerida - 2.395 respondentes')
+elif (Cidade == '63'):
+    print ('\nVeja os dados de Palmas! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 306.296 habitantes \n Tamanho da amostra sugerida - 2.383 respondentes')
+elif (Cidade == '51'):
+    print ('\nVeja os dados de Porto Alegre! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 1.488.252 habitantes \n Tamanho da amostra sugerida - 2.398 respondentes')
+elif (Cidade == '69'):
+    print ('\nVeja os dados de Porto Velho! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 539.354 habitantes \n Tamanho da amostra sugerida - 2.391 respondentes')
+elif (Cidade == '81'):
+    print ('\nVeja os dados de Recife! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 1.653.461 habitantes \n Tamanho da amostra sugerida - 2.398 respondentes')
+elif (Cidade == '68'):
+    print ('\nVeja os dados de Rio Branco! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 413.418 habitantes \n Tamanho da amostra sugerida - 2.388 respondentes')
+elif (Cidade == '21'):
+    print ('\nVeja os dados de Rio de Janeiro! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 6.747.815 habitantes \n Tamanho da amostra sugerida - 2.401 respondentes')
+elif (Cidade == '71'):
+    print ('\nVeja os dados de Salvador! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 2.886.698 habitantes \n Tamanho da amostra sugerida - 2.400 respondentes')
+elif (Cidade == '98'):
+    print ('\nVeja os dados de São Luís! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 1.108.975 habitantes \n Tamanho da amostra sugerida - 2.396 respondentes')
+elif (Cidade == '11'):
+    print ('\nVeja os dados de São Paulo! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 12.325.232 habitantes \n Tamanho da amostra sugerida - 2.401 respondentes')
+elif (Cidade == '86'):
+    print ('\nVeja os dados de Teresina! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 868.075 habitantes \n Tamanho da amostra sugerida - 2.395 respondentes')
+elif (Cidade == '27'):
+    print ('\nVeja os dados de Vitória! \n\n Grau de confiança - 95%  \n Margem de erro - 2% \n População em 2020 (IBGE) - 365.855 habitantes \n Tamanho da amostra sugerida - 2.386 respondentes')
+else:
+    print ("Código inválido. \n Por favor verifique o código da cidade de interesse novamente.")
